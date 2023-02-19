ac5477b7f88103f059f4fbccc25dd5ad0e79b9d1
nisabmohd <nisabmohd@gmail.com>
2023-02-20 00:51:15 +0530

    useAuthentication added

--- src/hooks/useAuthentication.ts
+++ src/hooks/useAuthentication.ts
@@ -1,5 +1,49 @@
-import {} from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  User,
+  signInWithEmailAndPassword,
+  signOut as signout,
+} from "firebase/auth";
+const auth = getAuth();
 
 export default function useAuthentication() {
-  return null;
+  function signUp(
+    email: string,
+    password: string,
+    onSuccess: (user: User) => void,
+    onFailure?: (err: any) => void
+  ) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        onSuccess(userCredential.user);
+      })
+      .catch((error) => {
+        if (onFailure) onFailure(error);
+      });
+  }
+  function signIn(
+    email: string,
+    password: string,
+    onSuccess: (user: User) => void,
+    onFailure?: (err: any) => void
+  ) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        onSuccess(userCredential.user);
+      })
+      .catch((error) => {
+        if (onFailure) onFailure(error);
+      });
+  }
+  function signOut(onSuccess: () => void, onFailure?: (err: any) => void) {
+    signout(auth)
+      .then(() => {
+        onSuccess();
+      })
+      .catch((err) => {
+        if (onFailure) onFailure(err);
+      });
+  }
+  return { signIn, signUp, signOut };
 }
